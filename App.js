d11934e175df54ad56df1dbce83c15d615c08c24
Cupabletrout <jh.enterface@gmail.com>
2023-11-04 17:03:15 -0500

    app.js updates

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Enterface</Text>
+      <Text>Enterface LLC</Text>
       <StatusBar style="auto" />
     </View>
   );
